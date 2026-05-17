4aa44cd4106c29b33d9b9899a5155ce25bb186a7
devTighadouini <iiliaastighadouini@gmail.com>
2026-05-17 18:52:24 +0200

    Incio de proyecto

---
+++ src/main/java/com/ilias/sudoku_server/SudokuServerApplication.java
@@ -0,0 +1,13 @@
+package com.ilias.sudoku_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilias/sudoku_server/SudokuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilias.sudoku_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
